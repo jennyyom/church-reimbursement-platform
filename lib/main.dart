eef2d036a6bfec3e11c92e7063bec838cad74630
jyom <jyom@wgu.edu>
2026-06-09 14:02:44 -0700

    feat: add l10n structure with English, Korean, and Swahili translations

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'login_page.dart';
 import 'package:firebase_core/firebase_core.dart'; //firebase 가져오기
 import 'package:firebase_auth/firebase_auth.dart';
-import 'firebase_options.dart';                    //연결정보 가져오기
-import 'home_page.dart';
+import 'firebase_options.dart';    
+import 'pages/login_page.dart';                //연결정보 가져오기
+import 'pages/home_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();       //flutter 준비된 떄까지 기다리기
